d1c25eda994c15686a49da831b2ddc4b75737766
jp <jon@jpoffline.com>
2016-01-02 16:41:15 +0000

    rpi

--- progbar.py
+++ progbar.py
@@ -1,7 +1,42 @@
 
 
 
+class BAR():
+	def __init__(self, minValue = 0, maxValue = 1):
+		self.minValue = minValue
+		self.maxValue = maxValue
+		self.nbars = 50
+		self.dBar = (float(self.maxValue) - float(self.minValue) ) / float(self.nbars)
+		self.prog = 0
+		
 
+	def getNbars(self, value):
+		print value, self.dBar
+		return int(value * self.dBar)
+
+	def makeBar(self, currentValue):
+
+		currentNbars = self.getNbars(currentValue)
+		
+		bar = '['
+		for i in xrange(0, currentNbars):
+			bar += '-'
+		bar += '>'
+		for i in xrange(0, self.nbars - currentNbars):
+			bar += ' '
+		bar += ']'
+		self.barStr = bar
+
+	def getBar(self):
+
+		self.makeBar(self.prog)
+		self.prog += 1
+		return self.barStr
+bar = BAR(maxValue = 5)
+print bar.dBar
+for i in xrange(0, bar.maxValue):
+	print bar.getBar()
+ 	
 def makeBar(prog, tot):
 	perct = float(prog) / float(tot) * 100
 	perct = str(perct) + '%'
@@ -11,9 +46,12 @@ def makeBar(prog, tot):
 	bar += '>'
 	for i in xrange(0, tot - prog):
 		bar += ' '
-	bar += '] ' + perct + '\n'
+	bar += '] ' + perct
 	return bar
 
+def getChannelValue(channelID):
+	import random
+	return int(random.random() * 50 )
 
 def dance(nchanels = 10, nsteps = 14):
 	
@@ -24,17 +62,17 @@ def dance(nchanels = 10, nsteps = 14):
 	ERASE_LINE = '\x1b[2K'
 
 	dele = ''
-
-	for i in xrange(0, nchanels + 1):
+	nheader_lines = 1
+	for i in xrange(0, nchanels + nheader_lines):
 		dele += CURSOR_UP_ONE + ERASE_LINE
 
 	for j in xrange(0, nsteps):
 		
 		sys.stdout.write('time = ' + str(j) + '\n' )
 
-		for i in xrange(0, nchanels):
-			value = int(random.random() * maxValue)
-			sys.stdout.write( str(i) + ' ' + makeBar(value, maxValue) )
+		for channel in xrange(0, nchanels):
+			value = getChannelValue(channel)
+			sys.stdout.write( str(channel) + ' ' + makeBar(value, maxValue) +'\n' )
 
 		time.sleep(0.1)
 		
@@ -42,4 +80,4 @@ def dance(nchanels = 10, nsteps = 14):
 		if j < nsteps - 1:
 			sys.stdout.write(dele)
 
-dance()
+#dance()
